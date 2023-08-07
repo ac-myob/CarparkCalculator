3b6bb4bf52399603e7101110ea0e4f6cd1dd3980
Andrew Christabel <andrew.christabel@myob.com>
2023-08-07 11:41:10 +1000

    Standard rate correct calculates cost for 3+ hours

--- CarparkCalculator/Application/Rates/StandardRate.cs
+++ CarparkCalculator/Application/Rates/StandardRate.cs
@@ -8,14 +8,15 @@ public class StandardRate : IHourlyRate
     public Money CalculateCost(CarparkDuration carparkDuration)
     {
         var elapsedHours = (carparkDuration.Exit - carparkDuration.Entry).TotalHours;
-        var wholeHours = (decimal) Math.Round(elapsedHours);
+        var wholeDays = (carparkDuration.Exit.Date - carparkDuration.Entry.Date).Days;
+        var wholeHours = (decimal) elapsedHours;
 
         var totalCost = elapsedHours switch
         {
-            < 1 => wholeHours * 5,
-            < 2 => wholeHours * 10,
-            < 3 => wholeHours * 15,
-            _ => wholeHours * 20
+            <= 1 => wholeHours * 5,
+            <= 2 => wholeHours * 10,
+            <= 3 => wholeHours * 15,
+            _ => (1 + wholeDays) * 20
         };
 
         return new Money(totalCost);
